4594bdea19dd3fdc72603076c5d52433519c9976
Your Name <cgalvis21_@hotmail.com>
2023-04-05 14:39:19 -0500

    done

--- funcionamiento.py
+++ funcionamiento.py
@@ -40,33 +40,31 @@ def construct_path(path, start, end):
     return result
 
 # Definir el grafo con las conexiones entre las ciudades y sus distancias
-graph = {
-    "Madrid": [("Lisbon", 635), ("Paris", 1056), ("Barcelona", 621)],
-    "Barcelona": [("Madrid", 621), ("Paris", 831), ("Milan", 862), ("Lisbon", 1204)],
-    "Paris": [("Madrid", 1056), ("Barcelona", 831), ("Brussels", 265), ("Amsterdam", 430), ("Berlin", 878), ("Rome", 1105)],
-    "Brussels": [("Paris", 265), ("Amsterdam", 173), ("Berlin", 655), ("London", 321)],
-    "Amsterdam": [("Paris", 430), ("Brussels", 173), ("Berlin", 663), ("London", 358)],
-    "Berlin": [("Paris", 878), ("Brussels", 655), ("Amsterdam", 663), ("Prague", 280), ("Vienna", 665), ("Warsaw", 574)],
-    "Prague": [("Berlin", 280), ("Vienna", 330), ("Budapest", 530), ("Krakow", 537)],
-    "Vienna": [("Berlin", 665), ("Prague", 330), ("Budapest", 243), ("Rome", 1036)],
-    "Budapest": [("Prague", 530), ("Vienna", 243), ("Belgrade", 370), ("Sofia", 472), ("Bucharest", 605)],
-    "Rome": [("Paris", 1105), ("Vienna", 1036), ("Naples", 225)],
-    "Lisbon": [("Madrid", 635), ("Barcelona", 1204)],
-    "London": [("Brussels", 321), ("Amsterdam", 358), ("Dublin", 463)],
-    "Dublin": [("London", 463)],
-    "Milan": [("Barcelona", 862), ("Zurich", 222), ("Vienna", 719)],
-    "Zurich": [("Milan", 222), ("Vienna", 672)],
-    "Warsaw":[("Berlin",574)],
-    "Naples":[("Rome",225)],
-    "Belgrade": [("Budapest",370)],
-    "Sofia": [("Budapest",472)],
-    "Bucharest": [("Budapest",605)],
-    "Krakow": [("Prague",537)]
-}
+graph = {}
+
+with open("grafo_europa.txt") as file:
+    for line in file:
+        city1, city2, distance = line.strip().split(",")
+        distance = int(distance)
+        if city1 not in graph:
+            graph[city1] = []
+        if city2 not in graph:
+            graph[city2] = []
+        graph[city1].append((city2, distance))
+        graph[city2].append((city1, distance))
+
 
 
 
 # Pedir las ciudades al usuario
+print("Ingrese el nombre tal cual como se muestra en la pantalla")
+print(" ")
+print("Las ciudades son:")
+print("---------------------------------------------------------------------")
+print("Madrid, Barcelona, Paris, Brussels, Amsterdam, Berlin, Prague, Vienna")
+print("Budapest, Rome, Lisbon, London, Dublin, Milan, Zurich, Wasaw")
+print("Naples, Belgrade, Sofia, Bucharest, Krakow")
+print("---------------------------------------------------------------------")
 ciudad_origen = input("Ingrese la ciudad de origen: ")
 ciudad_destino = input("Ingrese la ciudad de destino: ")
 
