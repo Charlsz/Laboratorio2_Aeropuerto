87bb101c45e2f208680e1f6730f8f40b947626f5
Your Name <cgalvis21_@hotmail.com>
2023-04-05 15:29:40 -0500

    fix

--- interfaz.py
+++ interfaz.py
@@ -2,28 +2,18 @@ import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
-from funcionamiento import dijkstra
+from funcionamiento import *
 
 # Definir el grafo con las conexiones entre las ciudades y sus distancias
-graph = {}
-with open("grafo_europa.txt") as file:
-    for line in file:
-        city1, city2, distance = line.strip().split(",")
-        distance = int(distance)
-        if city1 not in graph:
-            graph[city1] = []
-        if city2 not in graph:
-            graph[city2] = []
-        graph[city1].append((city2, distance))
-        graph[city2].append((city1, distance))
+graph = obtener_grafo()
 
 # Definir la aplicación Dash
 app = dash.Dash(__name__)
 
 # Definir el contenido HTML de la aplicación
 app.layout = html.Div([
-    html.H1('Calculadora de Rutas Europeas'),
-    html.P('Ingrese la ciudad de origen y destino para encontrar la ruta más corta entre ellas:'),
+    html.H1('Rutas Europeas'),
+    html.P('Ingrese la ciudad de origen y destino:'),
     dcc.Dropdown(
         id='ciudad_origen',
         options=[{'label': city, 'value': city} for city in graph.keys()],
@@ -57,6 +47,7 @@ def calcular_ruta(n_clicks, ciudad_origen, ciudad_destino):
         ])
     else:
         return html.P('Por favor, ingrese una ciudad de origen y una ciudad de destino y haga clic en "Calcular Ruta".')
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
